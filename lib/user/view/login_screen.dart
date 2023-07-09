9b56e4d24cfbfecbe8fcc5d5e2cf9eb6784cb9d9
IG <g9191hc@gmail.com>
2023-07-09 20:57:46 +0900

    간단한 로그인 시스템 만들어보기

--- lib/user/view/login_screen.dart
+++ lib/user/view/login_screen.dart
@@ -3,13 +3,22 @@ import 'dart:io';
 
 import 'package:actual/common/const/colors.dart';
 import 'package:actual/common/layout/default_layout.dart';
+import 'package:actual/common/view/root_tab.dart';
 import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 
 import '../../common/component/custom_text_form_field.dart';
 
-class LoginScreen extends StatelessWidget {
+class LoginScreen extends StatefulWidget {
   const LoginScreen({super.key});
+
+  @override
+  State<LoginScreen> createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  String username = '';
+  String password = '';
 
   @override
   Widget build(BuildContext context) {
@@ -38,19 +47,23 @@ class LoginScreen extends StatelessWidget {
                 ),
                 CustomTextFormField(
                   hintText: '이메일을 입력 해 주세요',
-                  onChanged: (value) {},
+                  onChanged: (value) {
+                    username = value;
+                  },
                 ),
                 const SizedBox(height: 16.0),
                 CustomTextFormField(
                   hintText: '비밀번호를을 입력 해 주세요',
-                  onChanged: (value) {},
+                  onChanged: (value) {
+                    password = value;
+                  },
                   obscureText: true,
                 ),
                 const SizedBox(height: 16.0),
                 ElevatedButton(
                   onPressed: () async {
                     //ID:Password
-                    final rawString = 'test@codefactory.ai:testtest';
+                    final rawString = '$username:$password';
 
                     Codec<String, String> stringToBase64 = utf8.fuse(base64);
 
@@ -65,6 +78,12 @@ class LoginScreen extends StatelessWidget {
                       ),
                     );
                     print(resp);
+
+                    Navigator.of(context).push(
+                      MaterialPageRoute(
+                        builder: (_) => RootTab(),
+                      ),
+                    );
                   },
                   child: Text('로그인'),
                   style: ElevatedButton.styleFrom(
@@ -73,8 +92,8 @@ class LoginScreen extends StatelessWidget {
                 ),
                 TextButton(
                   onPressed: () async {
-
-                    final token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6InRlc3RAY29kZWZhY3RvcnkuYWkiLCJzdWIiOiJmNTViMzJkMi00ZDY4LTRjMWUtYTNjYS1kYTlkN2QwZDkyZTUiLCJ0eXBlIjoicmVmcmVzaCIsImlhdCI6MTY4ODkwMjA1NiwiZXhwIjoxNjg4OTg4NDU2fQ.nP9119xwDFaIsKA6toJzjG9pksYzQCfzm8NAiAowbWk';
+                    final token =
+                        'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6InRlc3RAY29kZWZhY3RvcnkuYWkiLCJzdWIiOiJmNTViMzJkMi00ZDY4LTRjMWUtYTNjYS1kYTlkN2QwZDkyZTUiLCJ0eXBlIjoicmVmcmVzaCIsImlhdCI6MTY4ODkwMjA1NiwiZXhwIjoxNjg4OTg4NDU2fQ.nP9119xwDFaIsKA6toJzjG9pksYzQCfzm8NAiAowbWk';
 
                     final resp = await dio.post(
                       'http://$ip/auth/token',
