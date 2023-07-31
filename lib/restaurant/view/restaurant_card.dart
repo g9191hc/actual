cc0b66827a443061c9bfe60b3f72cb5ab864bdd4
IG <g9191hc@gmail.com>
2023-07-31 22:37:39 +0900

    Hero위젯

--- lib/restaurant/view/restaurant_card.dart
+++ lib/restaurant/view/restaurant_card.dart
@@ -5,9 +5,6 @@ import 'package:json_annotation/json_annotation.dart';
 
 import '../model/restaurant_model.dart';
 
-
-
-
 class RestaurantCard extends StatelessWidget {
   //이미지
   final Widget image;
@@ -33,7 +30,11 @@ class RestaurantCard extends StatelessWidget {
   //상세카드 여부
   final bool isDetail;
 
+  //상세 내용
   final String? detail;
+
+  //HeroKey
+  final String? heroKey;
 
   const RestaurantCard({
     super.key,
@@ -46,6 +47,7 @@ class RestaurantCard extends StatelessWidget {
     required this.ratings,
     this.isDetail = false,
     this.detail,
+    this.heroKey,
   });
 
   factory RestaurantCard.fromModel({
@@ -57,6 +59,7 @@ class RestaurantCard extends StatelessWidget {
         model.thumbUrl,
         fit: BoxFit.cover,
       ),
+      heroKey: model.id,
       name: model.name,
       tags: model.tags,
       ratingsCount: model.ratingsCount,
@@ -72,10 +75,17 @@ class RestaurantCard extends StatelessWidget {
   Widget build(BuildContext context) {
     return Column(
       children: [
-        if (isDetail) image,
-        if (!isDetail)
+        if (heroKey != null)
+          Hero(
+            tag: ObjectKey(heroKey),
+            child: ClipRRect(
+              borderRadius: BorderRadius.circular(isDetail ? 0.0 : 16.0),
+              child: image,
+            ),
+          ),
+        if (heroKey == null)
           ClipRRect(
-            borderRadius: BorderRadius.circular(16.0),
+            borderRadius: BorderRadius.circular(isDetail ? 0.0 : 16.0),
             child: image,
           ),
         const SizedBox(height: 16.0),
