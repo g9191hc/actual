40bd20246736385ab2fa3a1a4cb72a6e1efd489a
IG <g9191hc@gmail.com>
2023-08-15 19:31:57 +0900

    OrderProvider작업하기

---
+++ lib/order/provider/order_provider.dart
@@ -0,0 +1,46 @@
+import 'package:actual/order/model/post_order_body.dart';
+import 'package:actual/user/provider/basket_provider.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:uuid/uuid.dart';
+
+import '../model/order_model.dart';
+import '../repository/order_repository.dart';
+
+final orderProvider =
+    StateNotifierProvider<OrderStateNotifier, List<OrderModel>>(
+        (ref) => OrderStateNotifier(ref: ref));
+
+class OrderStateNotifier extends StateNotifier<List<OrderModel>> {
+  final Ref ref;
+
+  OrderStateNotifier({
+    required this.ref,
+  }) : super([]);
+
+  Future<bool> postOrder() async {
+    try {
+      final uuid = Uuid();
+      final id = uuid.v4();
+      final state = ref.read(basketProvider);
+      final repository = ref.read(orderRepositoryProvider);
+
+      final products = state
+          .map((e) =>
+              PostOrderBodyProduct(productId: e.product.id, count: e.count))
+          .toList();
+      final totalPrice = state.fold(0, (p, c) => p + c.count * c.product.price);
+
+      final resp = await repository.postOrder(
+        body: PostOrderBody(
+          id: id,
+          products: products,
+          totalPrice: totalPrice,
+          createdAt: DateTime.now.toString(),
+        ),
+      );
+      return true;
+    } on Exception catch (e) {
+      return false;
+    }
+  }
+}
--- lib/order/repository/order_repository.dart
+++ lib/order/repository/order_repository.dart
@@ -1,5 +1,5 @@
 import 'package:actual/order/model/order_model.dart';
-import 'package:actual/order/repository/post_order_body.dart';
+import 'package:actual/order/model/post_order_body.dart';
 import 'package:dio/dio.dart' hide Headers;
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:retrofit/http.dart';
