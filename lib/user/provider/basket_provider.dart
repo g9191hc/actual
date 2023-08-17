0c7368854c21580859162e9c8710a9228f2b2705
IG <g9191hc@gmail.com>
2023-08-18 01:50:26 +0900

    Debounce 적용하기

--- lib/user/provider/basket_provider.dart
+++ lib/user/provider/basket_provider.dart
@@ -3,6 +3,7 @@ import 'package:actual/product/model/product_model.dart';
 import 'package:actual/user/model/basket_item_model.dart';
 import 'package:actual/user/model/patch_basket_body.dart';
 import 'package:actual/user/repository/user_me_repository.dart';
+import 'package:debounce_throttle/debounce_throttle.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
@@ -10,16 +11,25 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:collection/collection.dart';
 
 final basketProvider =
-    StateNotifierProvider<BasketProvider, List<BasketItemModel>>((ref) {
+StateNotifierProvider<BasketProvider, List<BasketItemModel>>((ref) {
   final repository = ref.watch(userMeRepositoryProvider);
   return BasketProvider(repository: repository);
 });
 
 class BasketProvider extends StateNotifier<List<BasketItemModel>> {
   final UserMeRepository repository;
+  final updateBasketDebounce = Debouncer(
+    const Duration(seconds: 1),
+    initialValue: null,
+    checkEquality: false,
+  );
 
   //state 추가
-  BasketProvider({required this.repository}) : super([]);
+  BasketProvider({required this.repository}) : super([]) {
+    updateBasketDebounce.values.listen((state) {
+      patchBasket();
+    });
+  }
 
   //장바구니에 물건을 1개 추가하는 메서드
   //기존에 장바구니에 있었으면 count +=1(만약 있었는데 count가 1 미만이었으면 1로 변경), 없었으면 추가하면서 count = 1
@@ -31,13 +41,13 @@ class BasketProvider extends StateNotifier<List<BasketItemModel>> {
 
     if (exsits) {
       final exsistingProduct =
-          state.firstWhere((e) => e.product.id == product.id);
+      state.firstWhere((e) => e.product.id == product.id);
       if (exsistingProduct.count < 1) {
         state.map((e) => e.product.id == product.id ? e.copyWith(count: 1) : e);
       } else {
         state = state
             .map((e) =>
-                e.product.id == product.id ? e.copyWith(count: e.count + 1) : e)
+        e.product.id == product.id ? e.copyWith(count: e.count + 1) : e)
             .toList();
       }
     } else {
@@ -48,7 +58,8 @@ class BasketProvider extends StateNotifier<List<BasketItemModel>> {
     }
 
     //Optimistic Response
-    await patchBasket();
+    //Debouncer는 observable의 자손으로, setValue함수를 통해 등록되어 있는 listen메서드를 실행
+    updateBasketDebounce.setValue(null);
   }
 
   //장바구니에서 물건을 1개 빼거나 삭제하는 메서드
@@ -70,10 +81,11 @@ class BasketProvider extends StateNotifier<List<BasketItemModel>> {
     } else {
       state = state
           .map((e) =>
-              e.product.id == product.id ? e.copyWith(count: e.count - 1) : e)
+      e.product.id == product.id ? e.copyWith(count: e.count - 1) : e)
           .toList();
     }
-    await patchBasket();
+    //Optimistic Response
+    updateBasketDebounce.setValue(null);
   }
 
   Future<void> patchBasket() async {
@@ -81,11 +93,12 @@ class BasketProvider extends StateNotifier<List<BasketItemModel>> {
       body: PatchBasketBody(
         basket: state
             .map(
-              (e) => PatchBasketBodyBasket(
+              (e) =>
+              PatchBasketBodyBasket(
                 productId: e.product.id,
                 count: e.count,
               ),
-            )
+        )
             .toList(),
       ),
     );
